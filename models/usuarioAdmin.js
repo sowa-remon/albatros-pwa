7a33e2c221e5083112e6968de3d3e4fe3947aa2a
sowa <106477529+sowa-remon@users.noreply.github.com>
2025-02-05 12:13:40 -0600

    Agregar páginas de cada usuario

---
+++ models/usuarioAdmin.js
@@ -0,0 +1,24 @@
+class usuarioAdmin {
+    constructor(id, usuario, password, tipo) {
+        this.id = id;
+        this.usuario = usuario;
+        this.password = password;
+        this.tipo = tipo;
+    }
+    
+    // Método para transformar un documento de Firestore en un objeto de la clase
+    toFirestore(){
+        return {
+            usuario: this.usuario,
+            password: this.password,
+            tipo: this.tipo
+        }
+    }
+
+    static fromFirestore(doc){
+        const data = doc.data();
+        return new usuarioAdmin(doc.id, data.usuario, data.password, data.tipo);
+    }
+}
+
+module.exports = usuarioAdmin;
