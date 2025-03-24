285b35899740af7f167a03c9d505aa701b679809
sowa <106477529+sowa-remon@users.noreply.github.com>
2025-03-24 17:50:00 -0600

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,4 @@
-const { firestore } = require('./../../src/firebaseConfig');
+const { firestore } = require('./src/config/firebaseConfig');
 const express = require('express');
 const multer = require('multer');
 const bcrypt = require('bcrypt');
